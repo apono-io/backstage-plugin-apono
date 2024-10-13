5a218de9230922a4fe7abd4c0d54c8031e8bdbda
Dima Geyets <dima.g@apono.io>
2024-10-13 12:53:13 +0300

    Update Content-Security-Policy meta tag for client URL

--- src/components/AppWrapper/AppWrapper.tsx
+++ src/components/AppWrapper/AppWrapper.tsx
@@ -33,7 +33,7 @@ function CSPEffect({ clientUrl }: { clientUrl: string }) {
         meta.httpEquiv = "Content-Security-Policy";
         document.head.appendChild(meta);
       }
-      meta.content = `default-src 'self'; frame-src '${clientUrl}'`;
+      meta.content = `default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; frame-src ${clientUrl}`;
     } else if (meta) {
       document.head.removeChild(meta);
     }
