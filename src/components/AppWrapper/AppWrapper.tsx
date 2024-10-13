562b2a16165666bffb7b822ce1ccdc601f074d11
Dima Geyets <dima.g@apono.io>
2024-10-13 11:28:54 +0300

    Add Content-Security-Policy meta tag for client URL

--- src/components/AppWrapper/AppWrapper.tsx
+++ src/components/AppWrapper/AppWrapper.tsx
@@ -10,19 +10,40 @@ export function AppWrapper() {
   const config = useApi(configApiRef);
   const clientUrl = config.getOptionalString('apono.clientUrl') || defaultClientUrl;
 
+  return (
+    <>
+      <CSPEffect clientUrl={clientUrl} />
+      <Page themeId="tool" >
+        <Header title="Apono" subtitle="Automate access" />
+        <Content stretch noPadding>
+          <AponoIframe clientUrl={clientUrl} />
+        </Content>
+      </Page>
+    </>
+  );
+}
+
+function CSPEffect({ clientUrl }: { clientUrl: string }) {
   useEffect(() => {
-    const meta = document.createElement('meta');
-    meta.httpEquiv = "Content-Security-Policy";
-    meta.content = `default-src 'self'; frame-src ${clientUrl}`;
-    document.head.appendChild(meta);
+    let meta: HTMLMetaElement | null = document.querySelector('meta[http-equiv="Content-Security-Policy"]');
+
+    if (clientUrl) {
+      if (!meta) {
+        meta = document.createElement('meta');
+        meta.httpEquiv = "Content-Security-Policy";
+        document.head.appendChild(meta);
+      }
+      meta.content = `default-src 'self'; frame-src ${clientUrl}`;
+    } else if (meta) {
+      document.head.removeChild(meta);
+    }
+
+    return () => {
+      if (meta && document.head.contains(meta)) {
+        document.head.removeChild(meta);
+      }
+    };
   }, [clientUrl]);
 
-  return (
-    <Page themeId="tool" >
-      <Header title="Apono" subtitle="Automate access" />
-      <Content stretch noPadding>
-        <AponoIframe clientUrl={clientUrl} />
-      </Content>
-    </Page>
-  );
+  return null;
 }
