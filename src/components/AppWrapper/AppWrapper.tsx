d6161233dcebf384f57e96ea23d6567eaaf0b91e
Dima Geyets <dima.g@apono.io>
2024-10-13 11:24:40 +0300

    Add Content-Security-Policy meta tag for client URL

--- src/components/AppWrapper/AppWrapper.tsx
+++ src/components/AppWrapper/AppWrapper.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Content, Header, Page } from '@backstage/core-components';
 import { useApi,  configApiRef } from '@backstage/core-plugin-api';
 
@@ -9,6 +9,13 @@ const defaultClientUrl = 'https://backstage-client.apono.io';
 export function AppWrapper() {
   const config = useApi(configApiRef);
   const clientUrl = config.getOptionalString('apono.clientUrl') || defaultClientUrl;
+
+  useEffect(() => {
+    const meta = document.createElement('meta');
+    meta.httpEquiv = "Content-Security-Policy";
+    meta.content = `default-src 'self'; frame-src ${clientUrl}`;
+    document.head.appendChild(meta);
+  }, [clientUrl]);
 
   return (
     <Page themeId="tool" >
