bc08e86c34c394a85929926094468c6b7a6201dd
Dima Geyets <dima.g@apono.io>
2024-12-08 12:26:17 +0200

    Add support links to Apono configuration and update AppWrapper component

--- config.d.ts
+++ config.d.ts
@@ -5,5 +5,29 @@ export interface Config {
      * @visibility frontend
      */
     clientUrl?: string;
+
+    /**
+     * Support links
+     * @visibility frontend
+     */
+    supportLinks?: {
+      /**
+      * Support link label
+      * @visibility frontend
+      */
+      label: string;
+
+      /**
+      * Support link value
+      * @visibility frontend
+      */
+      value: string;
+
+      /**
+      * Support link URL
+      * @visibility frontend
+      */
+      url: string;
+    }[];
   };
 }
--- src/components/AppWrapper/AppWrapper.tsx
+++ src/components/AppWrapper/AppWrapper.tsx
@@ -10,6 +10,7 @@ const defaultClientUrl = 'https://backstage-client.apono.io';
 export function AppWrapper() {
   const config = useApi(configApiRef);
   const clientUrl = config.getOptionalString('apono.clientUrl') || defaultClientUrl;
+  const supportLinks = config.getOptionalConfigArray('apono.supportLinks') || [];
   const { profile, loading } = useProfile();
 
   return (
@@ -18,6 +19,9 @@ export function AppWrapper() {
         title="Apono"
         subtitle="Dynamic, just-in-time, just-enough access management that developers love."
       >
+        {supportLinks.map((link) => (
+          <HeaderLabel key={link.getString('label')} label={link.getString('label')} value={link.getString('value')} url={link.getString('url')} />
+        ))}
         <HeaderLabel label={profile?.displayName || 'Unknown'} value={profile?.email} />
       </Header>
       {!loading && (
